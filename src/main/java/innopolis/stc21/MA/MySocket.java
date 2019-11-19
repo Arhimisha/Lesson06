aa84a60c33cb93a5b5be548df5a863eb29eeb2ec
Mikhail Arkhipov <miklpostbox@gmail.com>
2019-11-20 00:07:37 +0300

    Refactor getPortFromConfig()

--- src/main/java/innopolis/stc21/MA/MySocket.java
+++ src/main/java/innopolis/stc21/MA/MySocket.java
@@ -14,44 +14,24 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class MySocket {
-    static final int DEFAULT_PORT = 8000;
-    static final String DEFAULT_IP = "127.0.0.1";
+    private static final int DEFAULT_PORT = 8000;
 
     public static void main(String[] args) {
-        int port = 0;
-        String ip = "";
-        try {
-            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            Document document = documentBuilder.parse("src\\main\\resources\\MySocket.config");
 
-            Node node = document.getDocumentElement();
-            NodeList elements = node.getChildNodes();
-            for (int i = 0; i < elements.getLength(); i++) {
-                Node element = elements.item(i);
-                if (element.getNodeType() != Node.TEXT_NODE && element.getNodeName() == "PORT") {
-                    port = Integer.valueOf(element.getChildNodes().item(0).getTextContent());
-
-                }
-                if (element.getNodeType() != Node.TEXT_NODE && element.getNodeName() == "IP") {
-                    ip = element.getChildNodes().item(0).getTextContent();
-                }
-            }
-        } catch (Throwable e) {
-            port = DEFAULT_PORT;
-            ip = DEFAULT_IP;
-        }
+        int port = getPortFromConfig();
 
         try {
             ServerSocket serverSocket = new ServerSocket(port);
             while (true) {
                 Socket socket = serverSocket.accept();
+
                 InputStream inputStream = socket.getInputStream();
                 OutputStream outputStream = socket.getOutputStream();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
                 //Writer writer = new OutputStreamWriter(outputStream);
                 System.out.println("clientRequest:");
                 String clientRequest = "";
-                while (clientRequest != null) {
+                while (!clientRequest.equals("\r\n")) {
                     clientRequest = reader.readLine();// +System.lineSeparator();
 
                     System.out.println( clientRequest);
@@ -63,5 +43,24 @@ public class MySocket {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static int getPortFromConfig() {
+        try {
+            DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document document = documentBuilder.parse("src\\main\\resources\\MySocket.config");
+
+            Node node = document.getDocumentElement();
+            NodeList elements = node.getChildNodes();
+            for (int i = 0; i < elements.getLength(); i++) {
+                Node element = elements.item(i);
+                if (element.getNodeType() != Node.TEXT_NODE && element.getNodeName().equals("PORT")) {
+                    return Integer.parseInt(element.getChildNodes().item(0).getTextContent());
+                }
+            }
+        } catch (Throwable e) {
+            return DEFAULT_PORT;
+        }
+        return DEFAULT_PORT;
     }
 }
